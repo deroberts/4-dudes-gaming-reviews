28cf1ab71aa179e8aafdc5a1e5c6e50b2532b8b8
deroberts <s501656@gmail.com>
2023-02-28 17:27:21 -0600

    boiler plate code commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+
+const Home = () => {
+ 
+
+  return (
+    <main>
+      <div className="">
+        <div className="">
+         hello
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+ 
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    // add profile is a mutation that takes in a name, email, and password and creates a new profile what we'd use to sign up
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    removeProfile: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOneAndDelete({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Make it so a logged in user can only remove a skill from their own profile
+    
+  },
+};
+
+module.exports = resolvers;
